2372fc1886dfc156fa032556a2355323c790a017
Mike <mike@nowhere.com>
2022-12-27 11:59:28 +0000

    Updating repo

--- wspace.py
+++ wspace.py
@@ -63,9 +63,51 @@ def wspaces(n_words):
 			S += P[k]
 		print(S)
 		
+def infstr(n_words):
+	# generate n_words using bbshub and convert to list
+	s0 = 14025256
+	sn = bbshub_gen(s0)
+	infstr = int_list(s0)
+	infstr += (int_list(sn))
+	n_words -= 2
+	while n_words:
+		sn = bbshub_gen(sn)
+		infstr += (int_list(sn))
+		n_words -= 1
+	n_ints = len(infstr)
+	# infstr[] has len() single digit integers
+	
+	width = 24
+	nxt = width	# index to additional integer from inf string
+	# ??advance the workspace until the first sum is not in dictionary??
+	# Initialise the workspace length 32
+	wspace = [infstr[0]]
+	for c in range(1,width+1):
+		wspace.append(wspace[-1]+infstr[c])
+		
+	# Loop start
+	p = dict()
+	p[0] = 0
+	posn = 1
+	print(wspace)		
+	while True:
+		# Process workspace
+		for i in wspace:
+			if i not in p:	# check for key
+				p[i] = posn
+		# update workspace
+		delta = wspace.pop(0) * -1
+		if nxt >= len(infstr):
+			break
+		wspace.append(wspace[-1]+infstr[nxt])
+		nxt += 1
+		wspace = list(map(lambda x : x + delta, wspace))
+		print(wspace)
+	
+#-----------------------------------------------------------------------
 		
 def main(args):
-	wspaces(80)
+	infstr(80)
 	return 0
 	
 	
